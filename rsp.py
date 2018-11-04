123940671860334f9a8113bb2420dc72c51cd435
Raj Das <rajdas.2001@icloud.com>
2018-11-04 08:18:32 +0530

    Update rsp.py

--- rsp.py
+++ rsp.py
@@ -1,5 +1,13 @@
 print "Enter Amount:"
 a = input()
+print "Number of Shares:"
+b=input()
+print "Number of Promotions:"
+c=input()
 rsp=0
 rsp=a/250
-print rsp
+r1=b*rsp
+r2=c*5
+tot=rsp+r2+r1
+
+print tot
