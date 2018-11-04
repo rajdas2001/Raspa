a40da590e5f88b842516d9c45d92d2b8dff0b0e1
Raj Das <rajdas.2001@icloud.com>
2018-11-04 07:46:55 +0530

    Add files via upload

---
+++ Raspa/index.js
@@ -0,0 +1,58 @@
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+
+    document.getElementById("user_div").style.display = "initial";
+    document.getElementById("login_div").style.display = "none";
+
+    var user = firebase.auth().currentUser;
+
+    if(user != null){
+
+      var email_id = user.email;
+      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+
+    }
+
+  } else {
+    // No user is signed in.
+
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("login_div").style.display = "initial";
+
+  }
+});
+
+function login(){
+
+  var userEmail = document.getElementById("emailId").value;
+  var userPass = document.getElementById("password").value;
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
+  });
+
+}
+
+function logout(){
+  firebase.auth().signOut();
+}
+
+function register(){
+	var mail = document.getElementById("email").value;
+  var adhar = document.getElementById("aadhar").value;
+	
+	firebase.auth().createUserWithEmailAndPassword(mail, adhar).catch(function(error) {
+	window.alert("Your aadhar number is your password.");
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ...
+});
+}
